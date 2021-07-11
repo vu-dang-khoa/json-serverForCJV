7a3317352539cee48ad0f3e5e64c15d1a4e4c2a1
vu-dang-khoa <vu-dang-khoa@myseneca.ca>
2021-07-11 00:58:51 -0400

    commit

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
+/*
+    Large part is taken from source :https://github.com/jesperorb/json-server-heroku
+*/
 server.use(middlewares);
 server.use(router);
 
